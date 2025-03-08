559cba3778b3cdd7109959945877229695862989
David <davidsarkisyanreal@gmail.com>
2025-03-09 00:32:14 +0300

    Backup

--- Objects/Level Objects/Spawner/spawner.gd
+++ Objects/Level Objects/Spawner/spawner.gd
@@ -1,11 +1,14 @@
 class_name Spawner extends Node2D
 
-##Variable name is descriptive enough.
+## Variable name is descriptive enough.
 @export var object_to_spawn: PackedScene
-##How many objects can be spawned. 0 means it will spit objects indefinitely.
+## How many objects can be spawned. 0 means it will spit objects indefinitely.
 @export var object_limit: int = 0
-##Delay between each spawn() call in seconds. 0 means it will spawn immediately.
+## Delay between each spawn() call in seconds. 0 means it will spawn immediately.
 @export var spawn_delay: float = 0.0
+
+@onready var visibility_notifier = $VisibilityNotifier
+@onready var spawn_delay_timer = $SpawnDelayTimer
 
 # Array to keep track of spawned objects
 var _object_list: Array = []
@@ -14,8 +17,12 @@ var _is_active: bool = false
 
 func set_active(active: bool):
 	_is_active = active
-	if $VisibilityNotifier.is_on_screen():
+	if !active: return
+	if visibility_notifier.is_on_screen():
 		spawn()
+		if spawn_delay > 0.0:
+			spawn_delay_timer.wait_time = spawn_delay
+			spawn_delay_timer.start()
 
 func spawn():
 	if object_to_spawn and _is_active: # Check if spawner is active and there's an object to spawn
@@ -33,3 +40,10 @@ func despawn():
 		for obj in _object_list:
 			obj.call_deferred("queue_free")
 			_object_list.erase(obj)
+
+func _on_screen_entered():
+	if _object_list.size() < object_limit:
+		spawn()
+
+func _on_spawn_delay_timeout():
+	pass # Replace with function body.
