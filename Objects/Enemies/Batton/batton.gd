4ec4e3f10282480faed04185cd17bdfcf324d5f5
David <davidsarkisyanreal@gmail.com>
2025-05-30 20:58:59 +0300

    Wood Man stage layout finished, Batton enemy added

---
+++ Objects/Enemies/Batton/batton.gd
@@ -0,0 +1,81 @@
+extends Enemy
+
+# SIGNALS
+
+# ENUMS
+enum STATES{
+	HIDING,
+	REVEALING,
+	CHASING,
+	RETREATING
+}
+
+# CONSTANTS
+
+# STATIC VARS
+
+# EXPORT VARS
+
+# ONREADY VARS
+@onready var vision_area: Area2D = $VisionArea
+
+# PUBLIC VARS
+
+# PRIVATE VARS
+
+# BUILT-IN METHODS
+func _enter_tree() -> void:
+	set_state(STATES.HIDING)
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	match _current_state:
+
+		STATES.HIDING:
+			pass
+
+		STATES.CHASING:
+			var move_dir = distance_between_player()
+
+			velocity = lerp(velocity, move_dir.normalized() * 50, 0.75)
+
+	move_and_slide()
+
+
+# METHODS
+func set_state(to_state: int) -> void:
+	if to_state == _current_state: return
+	match to_state:
+		0: # Hiding
+			pass
+		1: # Revealing
+			pass
+		2: # Chasing
+			sprite_controller.play_animation("fly")
+			%AnimationPlayer.play("bobbing")
+		3: # Retreating
+			pass
+
+	_current_state = to_state
+
+
+# SIGNAL METHODS
+func _on_vision_area_entered(body: Node2D) -> void:
+	if body is Player: self._player_reference = body
+	vision_area.set_deferred("monitoring", false)
+	sprite_controller.play_animation("reveal")
+
+
+func _on_sprite_animation_finished() -> void:
+	match $SpriteController/Anchor/Sprite.animation:
+		"reveal":
+			set_state(STATES.CHASING)
+
+
+func _on_screen_exited() -> void:
+	enemy_died.emit(self)
+	call_deferred("queue_free")
--- Objects/Enemies/enemy.gd
+++ Objects/Enemies/enemy.gd
@@ -8,11 +8,26 @@ signal enemy_died
 
 @export var hor_speed: int = 0
 
-enum STATES{}
 var _current_state: int
-
 var _player_reference: Player
 
-func look_at_player():
+
+func look_at_player() -> void:
 	if self.global_position.x > _player_reference.global_position.x: platform_component.set_direction(-1)
 	else: platform_component.set_direction(1)
+
+
+func distance_between_player() -> Vector2:
+	var vec2: Vector2
+
+	if self.global_position.x < _player_reference.global_position.x:
+		vec2.x = 1
+	else:
+		vec2.x = -1
+
+	if self.global_position.y < _player_reference.global_position.y:
+		vec2.y = 1
+	else:
+		vec2.y = -1
+
+	return vec2
